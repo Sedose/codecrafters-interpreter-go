65053d425efd5b250873ae0d450e5815f8c4cdcf
Edhar Avuzi <Edhar.EzenvaAvuzi@kingfisher.com>
2025-03-13 02:36:23 +0200

    Refactoring

--- app/main.go
+++ app/main.go
@@ -9,6 +9,12 @@ var validCommands = map[string]struct{}{
 	"tokenize": {},
 }
 
+type Token struct {
+	Type    string
+	Lexeme  string
+	Literal interface{}
+}
+
 func main() {
 	if len(os.Args) < 3 {
 		fmt.Fprintln(os.Stderr, "Usage: ./your_program.sh tokenize <filename>")
@@ -16,24 +22,31 @@ func main() {
 	}
 
 	command, filename := os.Args[1], os.Args[2]
-
-	if _, exists := validCommands[command]; !exists {
+	if _, ok := validCommands[command]; !ok {
 		fmt.Fprintf(os.Stderr, "Unknown command: %s\n", command)
 		os.Exit(1)
 	}
 
-	fileContents, err := os.ReadFile(filename)
+	contents, err := os.ReadFile(filename)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
 		os.Exit(1)
 	}
 
-	if command == "tokenize" {
-		ScanTokens(fileContents)
+	switch command {
+	case "tokenize":
+		tokens, err := scanTokens(contents)
+		for _, t := range tokens {
+			fmt.Printf("%s %s %v\n", t.Type, t.Lexeme, t.Literal)
+		}
+		fmt.Println("EOF  null")
+		if err != nil {
+			os.Exit(65)
+		}
 	}
 }
 
-func ScanTokens(input []byte) {
+func scanTokens(input []byte) ([]Token, error) {
 	tokenMap := map[byte]string{
 		'(': "LEFT_PAREN",
 		')': "RIGHT_PAREN",
@@ -47,21 +60,21 @@ func ScanTokens(input []byte) {
 		'*': "STAR",
 	}
 
-	hasError := false
+	var tokens []Token
+	var hasError bool
 
 	for _, symbol := range input {
-		if token, exists := tokenMap[symbol]; exists {
-			fmt.Printf("%s %c null\n", token, symbol)
+		if tokenType, ok := tokenMap[symbol]; ok {
+			tokens = append(tokens, Token{Type: tokenType, Lexeme: string(symbol), Literal: nil})
 		} else {
 			fmt.Fprintf(os.Stderr, "[line 1] Error: Unexpected character: %c\n", symbol)
 			hasError = true
 		}
 	}
 
-	fmt.Println("EOF  null")
-
-	// Exit with error code 65 if any errors were detected
 	if hasError {
-		os.Exit(65)
+		return tokens, fmt.Errorf("one or more scanning errors occurred")
 	}
+
+	return tokens, nil
 }
