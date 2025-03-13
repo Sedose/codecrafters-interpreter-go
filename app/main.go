b93b1295952fccff89a1d9935660999198834ac6
Edhar Avuzi <Edhar.EzenvaAvuzi@kingfisher.com>
2025-03-13 02:33:20 +0200

    Implement Scanning: Lexical errors

--- app/main.go
+++ app/main.go
@@ -34,23 +34,34 @@ func main() {
 }
 
 func ScanTokens(input []byte) {
+	tokenMap := map[byte]string{
+		'(': "LEFT_PAREN",
+		')': "RIGHT_PAREN",
+		'{': "LEFT_BRACE",
+		'}': "RIGHT_BRACE",
+		',': "COMMA",
+		'.': "DOT",
+		'-': "MINUS",
+		'+': "PLUS",
+		';': "SEMICOLON",
+		'*': "STAR",
+	}
+
+	hasError := false
+
 	for _, symbol := range input {
 		if token, exists := tokenMap[symbol]; exists {
 			fmt.Printf("%s %c null\n", token, symbol)
+		} else {
+			fmt.Fprintf(os.Stderr, "[line 1] Error: Unexpected character: %c\n", symbol)
+			hasError = true
 		}
 	}
-	fmt.Println("EOF  null")
-}
 
-var tokenMap = map[byte]string{
-	'(': "LEFT_PAREN",
-	')': "RIGHT_PAREN",
-	'{': "LEFT_BRACE",
-	'}': "RIGHT_BRACE",
-	',': "COMMA",
-	'.': "DOT",
-	'-': "MINUS",
-	'+': "PLUS",
-	';': "SEMICOLON",
-	'*': "STAR",
+	fmt.Println("EOF  null")
+
+	// Exit with error code 65 if any errors were detected
+	if hasError {
+		os.Exit(65)
+	}
 }
